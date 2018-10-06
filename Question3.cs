8c91693f0e21b7e8ea0e06faadb8948939ba896c
Jitendra9790 <43837472+Jitendra9790@users.noreply.github.com>
2018-10-06 10:55:28 +0530

    Add files via upload

---
+++ Question3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Q_2
+{
+    public class Shape
+    {
+       public virtual void WhoamI()
+        {
+            Console.WriteLine("I m Shape");
+        }
+    }
+     class Triangle : Shape
+     {
+        public override void WhoamI()
+        {
+            Console.WriteLine("I m Triangle");
+        }
+     }
+     public class Circle : Shape
+     {
+        //void WhoamI()[ERROR]
+        //( By putting new keyword, the parent class method is not overridden )
+
+        public new void WhoamI()
+        {
+            Console.WriteLine("I m Circle");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s;
+            s = new Triangle();
+            s.WhoamI();
+
+            s = new Circle();
+            s.WhoamI();
+
+            Console.ReadKey();
+        }
+    }
+}
